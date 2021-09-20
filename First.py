3023be8772e80196401542ae4161d13085c61874
Ranuga-D <79456372+Programmer-RD-AI@users.noreply.github.com>
2021-09-20 20:26:09 +0530

    Update First.py

--- First.py
+++ First.py
@@ -38,3 +38,138 @@ print("The default speed & direction of motor is LOW & Forward.....")
 print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
 print("\n")    
 a = 0
+while(1):
+    x = input('Enter : ')
+    
+    if x=='r':
+        GPIO.output(TRIG, True)
+        time.sleep(0.00001)
+        GPIO.output(TRIG, False)
+        while GPIO.input(ECHO) == False:
+            start = time.time()
+        while GPIO.input(ECHO) == True:
+            end = time.time()
+        sig_time = end-start
+        distance = sig_time / 0.000058
+        # distance = random.choice([1.0,10.0])
+        print(f'distance : {distance}')
+    
+        if distance <= 2500:
+            print('HIGH')
+            GPIO.output(OUT,GPIO.HIGH)
+        else:
+            GPIO.output(OUT,GPIO.LOW)
+        print("run")
+        GPIO.output(in1,GPIO.HIGH)
+        GPIO.output(in2,GPIO.LOW)
+        GPIO.output(in3,GPIO.HIGH)
+        GPIO.output(in4,GPIO.LOW)
+        x='z'
+        a = 1
+
+    elif x=='s':
+        print("stop")
+        GPIO.output(in1,GPIO.LOW)
+        GPIO.output(in2,GPIO.LOW)
+        GPIO.output(in3,GPIO.LOW)
+        GPIO.output(in4,GPIO.LOW)
+        x='z'
+
+    elif x=='f':
+        print("forward")
+        GPIO.output(in1,GPIO.HIGH)
+        GPIO.output(in2,GPIO.LOW)
+        GPIO.output(in3,GPIO.HIGH)
+        GPIO.output(in4,GPIO.LOW)
+        x='z'
+
+    elif x=='b':
+        GPIO.output(in1,GPIO.LOW)
+        GPIO.output(in2,GPIO.HIGH)
+        GPIO.output(in3,GPIO.LOW)
+        GPIO.output(in4,GPIO.HIGH)
+        x='z'
+
+    elif x=='l':
+        GPIO.output(TRIG, True)
+        time.sleep(0.00001)
+        GPIO.output(TRIG, False)
+        while GPIO.input(ECHO) == False:
+            start = time.time()
+        while GPIO.input(ECHO) == True:
+            end = time.time()
+        sig_time = end-start
+        distance = sig_time / 0.000058
+        # distance = random.choice([1.0,10.0])
+        print(f'distance : {distance}')
+    
+        if distance <= 2500:
+            print('HIGH')
+            GPIO.output(OUT,GPIO.HIGH)
+        else:
+            GPIO.output(OUT,GPIO.LOW)
+        print("low")
+        p1.ChangeDutyCycle(25)
+        p2.ChangeDutyCycle(25)
+        x='z'
+
+    elif x=='m':
+        print("medium")
+        GPIO.output(TRIG, True)
+        time.sleep(0.00001)
+        GPIO.output(TRIG, False)
+        while GPIO.input(ECHO) == False:
+            start = time.time()
+        while GPIO.input(ECHO) == True:
+            end = time.time()
+        sig_time = end-start
+        distance = sig_time / 0.000058
+        # distance = random.choice([1.0,10.0])
+        print(f'distance : {distance}')
+    
+        if distance <= 2500:
+            print('HIGH')
+            GPIO.output(OUT,GPIO.HIGH)
+        else:
+            GPIO.output(OUT,GPIO.LOW)
+        p1.ChangeDutyCycle(50)
+        p2.ChangeDutyCycle(50)
+        x='z'
+
+    elif x=='h':
+        GPIO.output(TRIG, True)
+        time.sleep(0.00001)
+        GPIO.output(TRIG, False)
+        while GPIO.input(ECHO) == False:
+            start = time.time()
+        while GPIO.input(ECHO) == True:
+            end = time.time()
+        sig_time = end-start
+        distance = sig_time / 0.000058
+        # distance = random.choice([1.0,10.0])
+        print(f'distance : {distance}')
+    
+        if distance <= 2500:
+            print('HIGH')
+            GPIO.output(OUT,GPIO.HIGH)
+        else:
+            GPIO.output(OUT,GPIO.LOW)
+        print("high")
+        p1.ChangeDutyCycle(100)
+        p2.ChangeDutyCycle(100)
+        x='z'
+     
+    
+    
+    elif x=='e':
+        GPIO.cleanup()
+        print("GPIO Clean up")
+        break
+    
+    else:
+        print("<<<  wrong data  >>>")
+        print("please enter the defined data to continue.....")
+
+
+
+
